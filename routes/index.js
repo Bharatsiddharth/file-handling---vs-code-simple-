aed1e9df4b8c4fbcdf39a8197f6518535dd83c93
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-03-14 23:10:38 +0530

    second commit

--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,22 @@ var globalpath = path.join(__dirname, "../", "public", "uploads")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  const files = fs.readdirSync(globalpath);
+  res.render('index', {files:files});
 });
+
+router.get('/:filename', function(req, res, next) {
+  const files = fs.readdirSync(globalpath);
+  res.render('index', {files: files});
+});
+
 
 router.post('/createfile', function(req, res, next) {
   var filename = req.body.filename;
   fs.writeFileSync(path.join(globalpath, filename), "");
-  res.send("file created")
+  // res.send("file created")
+
+  res.redirect(`${filename}`)
 });
 
 module.exports = router;
