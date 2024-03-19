07766b3b974c2a7f61502b45a9085f776fe2514f
Bharatsiddharth <bharatsiddharathsingh@gmail.com>
2024-03-19 23:30:49 +0530

    update added

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,14 @@ var globalpath = path.join(__dirname, "../", "public", "uploads")
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const files = fs.readdirSync(globalpath);
-  res.render('index', {files:files, filedata: ""});
+  res.render('index', {files:files, filedata: "", filename:''});
 });
 
 router.get('/:filename', function(req, res, next) {
 
   const filedata = fs.readFileSync(path.join(globalpath, req.params.filename), "utf-8");
   const files = fs.readdirSync(globalpath);
-  res.render('index', {files: files, filedata : filedata});
+  res.render('index', {files: files, filedata : filedata, filename:req.params.filename});
 });
 
 
@@ -34,6 +34,12 @@ router.get('/delete/:filename', function(req, res, next) {
   fs.unlinkSync(path.join(globalpath, req.params.filename));
   res.redirect("/")
   
+});
+
+router.post('/update/:filename', function(req, res, next) {
+
+  fs.writeFileSync(path.join(globalpath, req.params.filename),req.body.filedata)
+  res.redirect(`/${req.params.filename}`);
 });
 
 module.exports = router;
